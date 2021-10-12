d4e61a42bc3f699f2097c0deb200273cbc9ea8a8
Danial Arab <54812742+DanialArab@users.noreply.github.com>
2021-10-12 08:43:09 -0700

    Create Multi_class_semantic_segmentation_code.py

---
+++ Multi_class_semantic_segmentation_code.py
@@ -0,0 +1,238 @@
+
+Created on Mon Oct 11 21:39:09 2021
+
+@author: Danial Arab
+"""
+# Step_1: Importing the required libraries
+    
+import tensorflow as tf
+import keras.utils
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical 
+from sklearn.utils import class_weight
+from keras.metrics import MeanIoU
+import random
+import os
+import glob
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+SIZE_X = 128 
+SIZE_Y = 128
+n_classes=3 
+
+# Step_2: Building the model 
+
+def multiclass_unet_model(n_classes, IMG_HEIGHT=128, IMG_WIDTH=128, IMG_CHANNELS=3):
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = inputs
+
+    # Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    # Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    return model
+ 
+# Step_3: Data handling (importing images and the corresponding masks as a list, 
+# resize them, and convert the list into an array)
+train_images = []
+
+for directory_path in glob.glob("resized_images/"):
+    for img_path in glob.glob(os.path.join(directory_path, "*.jpg")):
+        img = cv2.imread(img_path)       
+        img = cv2.resize(img, (SIZE_Y, SIZE_X))
+        train_images.append(img)
+       
+train_images = np.stack (train_images, axis = 0)
+
+train_masks = [] 
+for directory_path in glob.glob("resized_masks/"):
+    for mask_path in glob.glob(os.path.join(directory_path, "*.png")):
+        mask = cv2.imread(mask_path, 0)       
+        mask = cv2.resize(mask, (SIZE_Y, SIZE_X), interpolation = cv2.INTER_NEAREST)
+        train_masks.append(mask)
+        
+train_masks = np.stack(train_masks, axis = 0)
+
+# Step_4: Encoding the labels to be started from 0, to do so we need to flatten first, becasue labelencoder works only on 
+# single vector, then encode the labels and finally reshape it back to the original shape 
+
+labelencoder = LabelEncoder()
+n, h, w = train_masks.shape
+train_masks_reshaped = train_masks.reshape(-1,1)
+train_masks_reshaped_encoded = labelencoder.fit_transform(train_masks_reshaped)
+train_masks_encoded_original_shape = train_masks_reshaped_encoded.reshape(n, h, w)
+
+np.unique(train_masks_encoded_original_shape)
+
+train_images = np.expand_dims(train_images, axis=4)
+train_images = tf.keras.utils.normalize(train_images, axis=1)
+
+train_masks_input = np.expand_dims(train_masks_encoded_original_shape, axis=3)
+
+# Step_5: Splitting the dataset 
+X1, X_test, y1, y_test = train_test_split(train_images, train_masks_input, test_size = 0.10, random_state = 0)
+
+# Further splitting training data to a smaller subset for quick testing of models
+X_train, X_do_not_use, y_train, y_do_not_use = train_test_split(X1, y1, test_size = 0.2, random_state = 0)
+
+# Step_6: Converting to categorical 
+train_masks_cat = to_categorical(y_train, num_classes=n_classes)
+y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], n_classes))
+
+test_masks_cat = to_categorical(y_test, num_classes=n_classes)
+y_test_cat = test_masks_cat.reshape((y_test.shape[0], y_test.shape[1], y_test.shape[2], n_classes))
+
+class_weights = class_weight.compute_class_weight('balanced',
+                                                 np.unique(train_masks_reshaped_encoded),
+                                                 train_masks_reshaped_encoded)
+print("Class weights are...:", class_weights)
+
+
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH  = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3]
+
+def get_model():
+    return multiclass_unet_model (n_classes=n_classes, IMG_HEIGHT=IMG_HEIGHT, IMG_WIDTH=IMG_WIDTH, IMG_CHANNELS=IMG_CHANNELS)
+
+model = get_model()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# Step_7: Training the model 
+
+history = model.fit(X_train, y_train_cat, 
+                    batch_size = 16, 
+                    verbose=1, 
+                    epochs=50, 
+                    validation_data=(X_test, y_test_cat), 
+                    #class_weight=class_weights,
+                    shuffle=False)
+                    
+
+model.save('model.hdf5')
+
+# Step_8: Evaluate the model performance
+
+_, acc = model.evaluate(X_test, y_test_cat)
+print("Accuracy is = ", (acc * 100.0), "%")
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation Accuracy')
+plt.title('Training and validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# Calculating IoU
+y_pred=model.predict(X_test)
+y_pred_argmax=np.argmax(y_pred, axis=3)
+
+n_classes = 3
+IOU_keras = MeanIoU(num_classes=n_classes)  
+IOU_keras.update_state(y_test[:,:,:,0], y_pred_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+# I0U for each class
+values = np.array(IOU_keras.get_weights()).reshape(n_classes, n_classes)
+print(values) 
+class1_IoU = values[0,0]/(values[0,0] + values[0,1] + values[0,2] + values[1,0]+ values[2,0])
+class2_IoU = values[1,1]/(values[1,1] + values[1,0] + values[1,2]  + values[0,1]+ values[2,1])
+class3_IoU = values[2,2]/(values[2,2] + values[2,0] + values[2,1] + values[0,2]+ values[1,2])
+
+print("IoU for class1 is: ", class1_IoU)
+print("IoU for class2 is: ", class2_IoU)
+print("IoU for class3 is: ", class3_IoU)
+
+# Step_9: Prediction on some random images 
+
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,:,0][:,:,:,None]
+
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input))
+predicted_img=np.argmax(prediction, axis=3)[0,:,:]
+
+plt.figure(figsize=(12, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='jet')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(predicted_img, cmap='jet')
+plt.show()
